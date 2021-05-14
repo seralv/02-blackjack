14383b8876b7ee882303af8e2a2d6ce52616b2a4
gru-sergio <gru.sergio@protonmail.com>
2021-05-13 22:16:48 -0400

    Update to valorCarta, watch deck.pop()

--- assets/js/juego.js
+++ assets/js/juego.js
@@ -15,9 +15,30 @@ const crearDeck = () => {
         }
     }
 
-    console.log( deck );
+    // console.log( deck );
     deck = _.shuffle( deck );
     console.log( deck );
 }
 
 crearDeck();
+
+const pedirCarta = () => {
+    if ( deck.length === 0 ) {
+        throw 'No hay cartas en el deck';
+    }
+
+    const carta = deck.pop();
+
+    // console.log( deck );
+    console.log( carta );
+    return carta;
+}
+
+pedirCarta();
+
+const valorCarta = ( carta ) => {
+    const valor = carta.substring( 0, carta.length-1 );
+    return isNaN(valor) ? ( ( valor === 'A' ) ? 11 : 10 ) : valor * 1;
+}
+
+console.log(valorCarta( pedirCarta() ));
